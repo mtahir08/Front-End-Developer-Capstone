5a18b11350dcb6b82bb98a9a24eb2b3fefc7272a
Muhammad Tahir <mtahir.ps@gmail.com>
2023-03-18 21:58:40 +0500

    fix: mobile responsive

--- src/layouts/NavBar.js
+++ src/layouts/NavBar.js
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/Logo.svg';
 import './NavBar.css';
 
 function NavBar() {
+  const navRef = useRef(null);
+
+  const toggleNav = () => {
+    navRef.current.classList.toggle('active');
+  };
+
   return (
     <nav>
       <Link to='/'>
         <img src={Logo} alt='Little Lemon Logo' />
       </Link>
-      <ul>
+      <button onClick={toggleNav}>☰</button>
+      <ul ref={navRef}>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to='/' onClick={toggleNav}>
+            Home
+          </Link>
         </li>
         <li>
-          <a href='#about'>About</a>
+          <a href='#about' onClick={toggleNav}>
+            About
+          </a>
         </li>
         <li>
-          <a href='#menu'>Menu</a>
+          <a href='#menu' onClick={toggleNav}>
+            Menu
+          </a>
         </li>
         <li>
-          <Link to='/booking'>Reservations</Link>
+          <Link to='/booking' onClick={toggleNav}>
+            Reservations
+          </Link>
         </li>
         <li>
-          <a href='#order-online'>Order Online</a>
+          <a href='#order-online' onClick={toggleNav}>
+            Order Online
+          </a>
         </li>
         <li>
-          <a href='#login'>Login</a>
+          <a href='#login' onClick={toggleNav}>
+            Login
+          </a>
         </li>
       </ul>
     </nav>
