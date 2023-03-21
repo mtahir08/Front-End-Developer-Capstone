7bafeec5ac1f9e056c915d25b1aaa7c38a739c7d
Hassan Masood <iamhassanmasood@gmail.com>
2023-03-21 15:53:42 +0500

    feat: style fonts updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavBar from './layouts/NavBar';
-import Footer from './layouts/Footer';
-import Home from './pages/Home';
-import Booking from './pages/Booking';
-import BookingConfirmation from './pages/BookingConfirmation';
-import About from './pages/About';
-import Login from './pages/Login';
-import Order from './pages/Order';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./layouts/NavBar";
+import Footer from "./layouts/Footer";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+import BookingConfirmation from "./pages/BookingConfirmation";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Order from "./pages/Order";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/booking' element={<Booking />} />
-        <Route path='/booking-confirmation' element={<BookingConfirmation />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/order' element={<Order />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/booking-confirmation" element={<BookingConfirmation />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/order" element={<Order />} />
       </Routes>
       <Footer />
     </BrowserRouter>
